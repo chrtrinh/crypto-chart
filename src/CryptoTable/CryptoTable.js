31247100add23e1d7f1621b8832ea8ea04a4a90d
Christopher Trinh <chris.trinh@live.com>
2020-12-22 17:25:06 -0500

    Upload: Added CryptoTable Component

--- src/CryptoTable/CryptoTable.js
+++ src/CryptoTable/CryptoTable.js
@@ -2,10 +2,22 @@ import React from "react";
 import "./CryptoTable.css";
 import { DataGrid } from "@material-ui/data-grid";
 
+const Column = [
+	{ field: "id", headerName: "#", width: 70 },
+	{ field: "Name", headerName: "Name", width: 130 },
+	{ field: "Price", headerName: "Price", width: 130, type: number },
+	{ field: "24h", headerName: "24h", width: 130, type: number },
+	{ field: "7d", headerName: "7d", width: 130, type: number },
+	{ field: "Market Cap", headerName: "Market Cap", width: 130, type: number },
+];
+
+const Row = [{}];
+
 function CryptoTable() {
 	return (
 		<div className="cryptoTable">
 			<h1>CryptoTable</h1>
+			<DataGrid />
 		</div>
 	);
 }
